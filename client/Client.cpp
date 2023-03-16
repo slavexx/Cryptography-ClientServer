15816bad5749806a5a2acda9fb4b7dfdc749fc8c
Viachaslau Lepeshau <slavex001@tut.by>
2023-03-16 16:23:33 +0300

    Key agreement implemented

--- client/Client.cpp
+++ client/Client.cpp
@@ -2,12 +2,26 @@
 #include <iostream>  
 #include <boost/asio.hpp>
 
+#include <cryptopp/osrng.h>
+#include <cryptopp/aes.h>
+#include <cryptopp/eccrypto.h>
+#include <cryptopp/secblock.h>
+#include <cryptopp/oids.h>
+#include <cryptopp/integer.h>
+
 using namespace boost::asio;
+using namespace CryptoPP;
 
 int main() {
-    boost::asio::io_service io_service;
-    std::string encryptedMsg, plainText;
 
+    auto readString = [](ip::tcp::socket& socket_) -> std::string {
+        boost::asio::streambuf buf;
+        boost::asio::read_until(socket_, buf, "\0");
+        std::string data = boost::asio::buffer_cast<const char*>(buf.data());
+        return data;
+    };
+
+    boost::asio::io_service io_service;
     //socket creation
     ip::tcp::socket socket(io_service);
 
@@ -15,14 +29,44 @@ int main() {
     socket.connect(ip::tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 1234));
 
     boost::system::error_code error;
-    std::string keyPart{ "testData" };
 
-    boost::asio::write(socket, boost::asio::buffer(keyPart), error);
+    //key generation
+
+    AutoSeededX917RNG<AES> rng;
+    //ASN1::secp256r1() tihs is common part with other side
+    ECDH<ECP>::Domain clientDomain(ASN1::secp256r1());
+
+    SecByteBlock privateKey(clientDomain.PrivateKeyLength()), publicKey(clientDomain.PublicKeyLength());
+
+    clientDomain.GenerateKeyPair(rng, privateKey, publicKey);
+    std::string publicClientKeyStr(reinterpret_cast<const char*>(publicKey.data()), publicKey.size());
+
+    boost::asio::write(socket, boost::asio::buffer(publicClientKeyStr), error);
     if (!error) {
         std::cout << "Key was sended successfully" << std::endl;
     }
     else {
         std::cout << "Key send failed: " << error.message() << std::endl;
+    }
+
+    //key agreement
+    std::string publicServerKeyStr = readString(socket);
+    //Anyway it will be aborted in line 65 (due to incorrect reading from the socket)
+    assert(publicServerKeyStr.size() == clientDomain.PublicKeyLength());
+    SecByteBlock publicServerKey(reinterpret_cast<const byte*>(publicServerKeyStr.data()), publicServerKeyStr.size());
+
+    SecByteBlock sharedKey(clientDomain.AgreedValueLength());
+
+    bool correctness = clientDomain.Agree(sharedKey, privateKey, publicServerKey);
+
+    Integer sharedKeyInt;
+    sharedKeyInt.Decode(sharedKey.BytePtr(), sharedKey.SizeInBytes());
+
+    if (correctness) {
+        std::cout << "We have a right shared key:" << sharedKeyInt;
+    }
+    else {
+        std::cout << "Error in shared key computation";
     }
 
     return 0;
--- server/Server.cpp
+++ server/Server.cpp
@@ -2,7 +2,16 @@
 #include <iostream>  
 #include <boost/asio.hpp>
 
+#include <cryptopp/osrng.h>
+#include <cryptopp/aes.h>
+#include <cryptopp/eccrypto.h>
+#include <cryptopp/secblock.h>
+#include <cryptopp/oids.h>
+#include <cryptopp/asn.h>
+#include <cryptopp/integer.h>
+
 using namespace boost::asio;
+using namespace CryptoPP;
 
 int main() {
 
@@ -14,7 +23,6 @@ int main() {
     };
 
     boost::asio::io_service io_service;
-
     //listen for new connection  
     ip::tcp::acceptor acceptor(io_service, ip::tcp::endpoint(ip::tcp::v4(), 1234));
 
@@ -24,7 +32,47 @@ int main() {
     //waiting for the connection  
     acceptor.accept(socket);
 
-    std::cout << readString(socket) << '\n';
+    //key generation
+
+    AutoSeededX917RNG<AES> rng;
+    //ASN1::secp256r1() tihs is common part with other side
+    ECDH<ECP>::Domain serverDomain(ASN1::secp256r1());
+
+    SecByteBlock privateKey(serverDomain.PrivateKeyLength()), publicKey(serverDomain.PublicKeyLength());
+
+    serverDomain.GenerateKeyPair(rng, privateKey, publicKey);
+    std::string publicServerKeyStr(reinterpret_cast<const char*>(publicKey.data()), publicKey.size());
+
+    boost::system::error_code error;
+
+    boost::asio::write(socket, boost::asio::buffer(publicServerKeyStr), error);
+    if (!error) {
+        std::cout << "Key was sended successfully" << std::endl;
+    }
+    else {
+        std::cout << "Key send failed: " << error.message() << std::endl;
+    }
+
+    //key agreement
+    std::string publicClientKeyStr = readString(socket);
+    //Anyway it will be aborted in line 65 (due to incorrect reading from the socket)
+    assert(publicClientKeyStr.size() == serverDomain.PublicKeyLength());
+
+    SecByteBlock publicClientKey(reinterpret_cast<const byte*>(publicClientKeyStr.data()), publicClientKeyStr.size());
+
+    SecByteBlock sharedKey(serverDomain.AgreedValueLength());
+
+    bool correctness = serverDomain.Agree(sharedKey, privateKey, publicClientKey);
+
+    Integer sharedKeyInt;
+    sharedKeyInt.Decode(sharedKey.BytePtr(), sharedKey.SizeInBytes());
+
+    if (correctness) {
+        std::cout << "We have a right shared key:" << sharedKeyInt;
+    }
+    else {
+        std::cout << "Error in shared key computation";
+    }
 
     return 0;
 }
