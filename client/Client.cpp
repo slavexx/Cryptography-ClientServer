1e6dae4a7eea6d66bd37afa233bc7151c00bbb62
Viachaslau Lepeshau <slavex001@tut.by>
2023-03-16 13:18:50 +0300

    Removed unnecessary code

--- client/Client.cpp
+++ client/Client.cpp
@@ -2,54 +2,7 @@
 #include <iostream>  
 #include <boost/asio.hpp>
 
-#include <Encryptor.h>
-
 using namespace boost::asio;
-
-void sendMessageAndMetainfo(
-                ip::tcp::socket& socket_,
-                Encryptor& encryptor_,
-                const std::string& authData_,
-                const std::string& message_)
-{
-    boost::system::error_code error;
-
-    //sending key
-    boost::asio::write(socket_, boost::asio::buffer(encryptor_.keyToString()), error);
-    if (!error) {
-        std::cout << "Key was sended successfully" << std::endl;
-    }
-    else {
-        std::cout << "Key send failed: " << error.message() << std::endl;
-    }
-    Sleep(300);
-    //sending initial vector
-    boost::asio::write(socket_, boost::asio::buffer(encryptor_.InitVecToString()), error);
-    if (!error) {
-        std::cout << "IV was sended successfully" << std::endl;
-    }
-    else {
-        std::cout << "IV send failed: " << error.message() << std::endl;
-    };
-    Sleep(300);
-    //sending authentification data
-    boost::asio::write(socket_, boost::asio::buffer(authData_), error);
-    if (!error) {
-        std::cout << "Authentification data was sended successfully" << std::endl;
-    }
-    else {
-        std::cout << "send failed: " << error.message() << std::endl;
-    }
-    Sleep(300);
-    //sending encrypted message
-    boost::asio::write(socket_, boost::asio::buffer(message_), error);
-    if (!error) {
-        std::cout << "Message was sended successfully\n\n" << std::endl;
-    }
-    else {
-        std::cout << "send failed: " << error.message() << std::endl;
-    }
-}
 
 int main() {
     boost::asio::io_service io_service;
@@ -61,27 +14,16 @@ int main() {
     //connection
     socket.connect(ip::tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 1234));
 
-    // request/message from client
-    std::cin >> plainText;
+    boost::system::error_code error;
+    std::string keyPart{ "testData" };
 
-    const std::string authData("hashOfUsername");
-
-    Encryptor encryptor;
-    encryptor.generateComplexKey();
-
-    //New key, new InitVector
-    encryptedMsg = encryptor.encrypt(plainText, authData);
-    sendMessageAndMetainfo(socket, encryptor, authData, encryptedMsg);
-
-    //Old key, new InitVector
-    encryptor.regenerateInitVec();
-    encryptedMsg = encryptor.encrypt(plainText, authData);
-    sendMessageAndMetainfo(socket, encryptor, authData, encryptedMsg);
-
-    //New key, old InitVector
-    encryptor.regenerateKey();
-    encryptedMsg = encryptor.encrypt(plainText, authData);
-    sendMessageAndMetainfo(socket, encryptor, authData, encryptedMsg);
+    boost::asio::write(socket, boost::asio::buffer(keyPart), error);
+    if (!error) {
+        std::cout << "Key was sended successfully" << std::endl;
+    }
+    else {
+        std::cout << "Key send failed: " << error.message() << std::endl;
+    }
 
     return 0;
 }
--- server/Server.cpp
+++ server/Server.cpp
@@ -2,11 +2,9 @@
 #include <iostream>  
 #include <boost/asio.hpp>
 
-#include <Decryptor.h>
-
 using namespace boost::asio;
 
-std::string getPlainMessageFromSocket(ip::tcp::socket& socket_) {
+int main() {
 
     auto readString = [](ip::tcp::socket& socket_) -> std::string {
         boost::asio::streambuf buf;
@@ -14,21 +12,6 @@ std::string getPlainMessageFromSocket(ip::tcp::socket& socket_) {
         std::string data = boost::asio::buffer_cast<const char*>(buf.data());
         return data;
     };
-
-    //read operation
-    auto key        = readString(socket_);
-    auto initVec    = readString(socket_);
-    auto authData   = readString(socket_);
-    auto message    = readString(socket_);
-
-    Decryptor decryptor(key, initVec);
-    decryptor.showKeyAndInitVec();
-
-    return decryptor.decrypt(message, authData);
-}
-
-
-int main() {
 
     boost::asio::io_service io_service;
 
@@ -41,15 +24,7 @@ int main() {
     //waiting for the connection  
     acceptor.accept(socket);
 
-    //Attack the first and last byte of the encrypted data and tag(MAC)
-    //message[0] |= 0x0F;
-    //message[message.size() - 1] |= 0x0F;
-
-    std::cout << "Decrypted message with new key and new InitVector:\n" << getPlainMessageFromSocket(socket) << std::endl;
-
-    std::cout << "\n\nDecrypted message with old key and new InitVector:\n" << getPlainMessageFromSocket(socket) << std::endl;
-
-    std::cout << "\n\nDecrypted message with new key and old InitVector:\n" << getPlainMessageFromSocket(socket) << std::endl;
+    std::cout << readString(socket) << '\n';
 
     return 0;
 }
